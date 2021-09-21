26b578ef72c2d1cbb850fe4fd2b81615b605e7c6
wumoye <lee.neil60@gmail.com>
2021-09-22 07:42:41 +0900

    Update:setting.py and ovaas/model.User Add:session_module.set_session and session_module.set_session

--- ovaas/views.py
+++ ovaas/views.py
@@ -12,18 +12,19 @@ from utils.token_module import get_response_json_dict, get_data_from_token
 class PasswordAuthentication(View):
 
     def get(self, request):
-        username = 'lee'
-        password = '111'
-
-        user_in_database = User.objects.filter(username=username).exists()
-        if not user_in_database:
-            user = User.objects.create_user(username=username, password=password)
-            user.save()
-            return JsonResponse(get_response_json_dict(token='created', status_code=200, message="created"))
-        else:
-            print(f'user exists')
-
-            return JsonResponse(get_response_json_dict(token='exists', status_code=504, message="exists"))
+        # test code----start
+        # username = 'lee'
+        # password = '111'
+        #
+        # user_in_database = User.objects.filter(username=username).exists()
+        # if not user_in_database:
+        #     user = User.objects.create_user(username=username, password=password)
+        #     user.save()
+        #     return JsonResponse(get_response_json_dict(token='created', status_code=200, message="created"))
+        # else:
+        #     print(f'user exists')
+        #
+        #     return JsonResponse(get_response_json_dict(token='exists', status_code=504, message="exists"))
 
         # dict = get_data_from_token()
         # username = request.session.get('lee')
@@ -32,33 +33,37 @@ class PasswordAuthentication(View):
         # res = get_session(username)
         #
         # return JsonResponse(get_response_json_dict(status_code=0, message=res))
+        # test code----end
+        pass
 
     def post(self, request):
         received_data = json.loads(request.body.decode('utf-8'))
         username = received_data['user']
         password = received_data['password']
         token = ''
-        # user_in_database = User.objects.filter(username=username).exists()
-        # if not user_in_database:
-        #     return JsonResponse(get_response_json_dict(token=token, status_code=401, message="User Does not exist"))
-        #
-        # user = authenticate(request, username=username, password=password)
-        #
-        # if user is not None:
-        #     login(request, user)
-        #
-        #     token = user.token
-        #     val = {'is_login':True,'user': username}
-        #         res = set_session(username, val)
-        #
-        #     return JsonResponse(
-        #         get_response_json_dict(token=token, status_code=200, message=f"User exist,session{res}"))
-        # else:
-        #     return JsonResponse(
-        #         get_response_json_dict(token='', status_code=402, message="Invalid username or password"))
+        user_in_database = User.objects.filter(username=username).exists()
+        if not user_in_database:
+            return JsonResponse(get_response_json_dict(token=token, status_code=401, message="User Does not exist"))
 
-        val = {'is_login': 'true', 'user': username}
-        res = set_session(username, val)
+        user = authenticate(request, username=username, password=password)
 
-        return JsonResponse(
-            get_response_json_dict(token=token, status_code=200, message=f"User exist.Set session {res}"))
+        if user is not None:
+            login(request, user)
+
+            token = user.token
+            val = {'is_login': True, 'user': username}
+            res = set_session(username, val)
+
+            return JsonResponse(
+                get_response_json_dict(token=token, status_code=200, message=f"User exist,Set session {res}"))
+        else:
+            return JsonResponse(
+                get_response_json_dict(token='', status_code=402, message="Invalid username or password"))
+
+        # test code----start
+        # val = {'is_login': 'true', 'user': username}
+        # res = set_session(username, val)
+        #
+        # return JsonResponse(
+        #     get_response_json_dict(token=token, status_code=200, message=f"User exist.Set session {res}"))
+        # test code----end
